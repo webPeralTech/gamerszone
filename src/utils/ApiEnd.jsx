43ab0529da3cb7851b702abe4d2fc7c827cb5348
Nidhi-Vedaha <70514612+Nidhi-Vedaha@users.noreply.github.com>
2024-09-17 00:18:52 +0530

    changes in responsive=v1

--- src/utils/ApiEnd.jsx
+++ src/utils/ApiEnd.jsx
@@ -1,4 +1,4 @@
-// export const BackendUrl = "https://gameofjoys.vercel.app";
+export const BackendUrl = "https://gameofjoys.vercel.app";
 
 // <<< local >>>
-export const BackendUrl = "http://localhost:8080"; 
+// export const BackendUrl = "http://localhost:8080"; 
