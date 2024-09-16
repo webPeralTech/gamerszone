57b6f216bec21a517fbb1366b051eab2eb100088
Nidhi-Vedaha <70514612+Nidhi-Vedaha@users.noreply.github.com>
2024-09-16 10:20:41 +0530

    coursol removed

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,43 +1,61 @@
 const Carousel = () => {
     return (
-        <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
-            <div className="carousel-indicators">
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-            </div>
-            <div className="carousel-inner" style={{ height: "500px" }}>
-                <div className="carousel-item active">
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTeIXRNPVWZ6kfBOwqOu1UnA3qTNWt9Vt3Cw&s" className="d-block w-100" alt="..." />
-                    <div className="carousel-caption d-none d-md-block">
-                        <h5>First slide label</h5>
-                        <p>Some representative placeholder content for the first slide.</p>
+        // <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
+        //     <div className="carousel-indicators">
+        //         <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+        //         <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        //         <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        //     </div>
+        //     <div className="carousel-inner" style={{ height: "500px" }}>
+        //         <div className="carousel-item active">
+        //             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTeIXRNPVWZ6kfBOwqOu1UnA3qTNWt9Vt3Cw&s" className="d-block w-100" alt="..." />
+        //             <div className="carousel-caption d-none d-md-block">
+        //                 <h5>First slide label</h5>
+        //                 <p>Some representative placeholder content for the first slide.</p>
+        //             </div>
+        //         </div>
+        //         <div className="carousel-item">
+        //             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwsXdVrx2S4-HGb8dmMtz6rWsBwZYdGF9Okg&s" className="d-block w-100" alt="..." />
+        //             <div className="carousel-caption d-none d-md-block">
+        //                 <h5>Second slide label</h5>
+        //                 <p>Some representative placeholder content for the second slide.</p>
+        //             </div>
+        //         </div>
+        //         <div className="carousel-item">
+        //             <img src="https://images.unsplash.com/photo-1724268508328-33a9caeba771?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="d-block w-100" alt="..." />
+        //             <div className="carousel-caption d-none d-md-block">
+        //                 <h5>Third slide label</h5>
+        //                 <p>Some representative placeholder content for the third slide.</p>
+        //             </div>
+        //         </div>
+        //     </div>
+        //     <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+        //         <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        //         <span className="visually-hidden">Previous</span>
+        //     </button>
+        //     <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+        //         <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        //         <span className="visually-hidden">Next</span>
+        //     </button>
+        // </div>
+        <>
+            <section className="hero-section">
+                <div className="container">
+                    <div className="row align-items-center justify-content-center">
+                        <div className="col-12 col-md-6 col-lg-9 text-center">
+                            <div className="hero-content">
+                                <div className="intro text-center mb-5">
+                                    <span class="intro-text">Game Of Joys</span>
+                                    <h1 class="mt-4">Explore the Ultimate Hub for Multiple Online Games</h1>
+                                    <p>From Action-Packed Battles to Strategy &amp; and Puzzle Games, All in One Place</p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div className="carousel-item">
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwsXdVrx2S4-HGb8dmMtz6rWsBwZYdGF9Okg&s" className="d-block w-100" alt="..." />
-                    <div className="carousel-caption d-none d-md-block">
-                        <h5>Second slide label</h5>
-                        <p>Some representative placeholder content for the second slide.</p>
-                    </div>
-                </div>
-                <div className="carousel-item">
-                    <img src="https://images.unsplash.com/photo-1724268508328-33a9caeba771?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="d-block w-100" alt="..." />
-                    <div className="carousel-caption d-none d-md-block">
-                        <h5>Third slide label</h5>
-                        <p>Some representative placeholder content for the third slide.</p>
-                    </div>
-                </div>
-            </div>
-            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span className="visually-hidden">Previous</span>
-            </button>
-            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-                <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                <span className="visually-hidden">Next</span>
-            </button>
-        </div>
+            </section>
+        </>
+
     )
 }
 export default Carousel
